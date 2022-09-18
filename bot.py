62eb94ea9148f51e7ef30bbaaf716446ce38dbdb
Termonoid <termonoid@yandex.ru>
2022-09-19 01:03:36 +0300

    FIXME: импорт Params

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 
 from connection import connection
-from models import Request, Params
+from models import Params
 
 config = toml.load(open("./config.toml"))
 bot = Bot(config['token'])
