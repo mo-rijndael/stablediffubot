f32d44c7dc259f1c790283d804f425ad4bd18cf2
Mo Rijndael <git@rijndael.cc>
2022-12-14 12:50:37 +0300

    Команды /start и /help

--- bot.py
+++ bot.py
@@ -10,6 +10,24 @@ config = toml.load(open("./config.toml"))
 bot = Bot(config['token'])
 dispatcher = Dispatcher(bot)
 
+@dispatcher.message_handler(commands=['start'])
+async def start(message: Message):
+    await message.answer("Привет! Чтобы начать генерировать, просто напиши затравку для нейросети")
+
+@dispatcher.message_handler(commands=['help'])
+async def help_(message: Message):
+    await message.answer("Для генерации используется неофициально апи "
+                         "[этой](https://huggingface.co/spaces/stabilityai/stable-diffusion) демки"
+                         "\n\n"
+                         "Параметры по умолчанию:\n"
+                         "картинки: 4\n"
+                         "шаги: 45\n"
+                         "Guidance scale: 7.5\n"
+                         "seed: случайный\n"
+                         "изменение параметров: пока не реализовано :(",
+                         parse_mode='MARKDOWN')
+
+
 @dispatcher.message_handler(run_task=True)
 async def process_request(message: Message):
     if not message.text:
